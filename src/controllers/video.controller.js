0bd6ffd2e45a52f4d691136091705cdac514e6a1
HemantNavlani <hemant.navlani.0506@gmail.com>
2024-07-21 14:24:59 +0530

    getAllVideos, getVideobyId,togglePublishStatus controllers made

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -7,7 +7,63 @@ import {Like} from '../models/like.model.js'
 import {Comment} from '../models/comment.model.js'
 
 const getAllVideos = async(req,res)=>{
+    const {page=1,limit = 10,query,sortBy,sortType,userId} = req.query
+    console.log(userId);
+    const pipeline = []
+    if (query){
+        pipeline.push({
+            $search:{
+                index : "search-videos",
+                text:{
+                    query,
+                    path:[title,description]
+                }
+            }
+        })
+    }
+    if (userId){
+        pipeline.push({$match:{
+            owner : new mongoose.Types.ObjectId(userId)}})
+    }
+    pipeline.push({$match:{isPublished:true}})
 
+    if (sortBy && sortType){
+        pipeline.push({
+            $sort:{
+                [sortBy]:sortType==='asc'?1:-1
+            }
+        })
+    }else{
+        pipeline.push({$sort:{createdAt:-1}})
+    }
+
+    pipeline.push({
+        $lookup:{
+            from:"users",
+            localField:"owner",
+            foreignField : "_id",
+            as : "ownerDetails",
+            pipeline:[
+                {
+                    $project:{
+                        username:1,
+                        "avatar.url":1
+                    }
+                }
+            ]
+        }
+    },
+    {
+        $unwind:"$ownerDetails"
+    }
+)
+const videoAggregate = Video.aggregate(pipeline);
+const options = {
+    page: parseInt(page,10),
+    limit : parseInt(limit,10)
+}
+const video = await Video.aggregatePaginate(videoAggregate,options)
+return res.status(200).json(new ApiResponse(200,video,"Videos fetched successfully"));
 }
 
 const publishAVideo = async(req,res)=>{
@@ -48,7 +104,111 @@ const publishAVideo = async(req,res)=>{
 }
 
 const getVideoById = async(req,res)=>{
+    const {videoId} = req.params;
+    const video = Video.aggregate([
+        {
+            $match:{
+                id: new mongoose.Types.ObjectId(videoId)
+            },
+        },
+        {
+            $lookup:{
+                from:"likes",
+                localField:"_id",
+                foreignField:"video",
+                as:"likes"
+            }
+        },
+        {
+            $lookup:{
+                from:"users",
+                localField:"owner",
+                foreignField:"_id",
+                as : "owner",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"subscriptions",
+                            localField:'_id',
+                            foreignField:'channel',
+                            as:"subscribers"
+                        }
+                    },
+                    {
+                        $addFields:{
+                            subscriberCount:{
+                                $size:"$subscribers",
+                            },
+                            isSubscribed:{
+                                $cond:{
+                                    if : {
+                                        $in:[
+                                            req.user?._id,
+                                            "$subscribers.subscriber",
+                                        ]
+                                    },
+                                    then:true,
+                                    else : false,
+                                }
+                            }
+                        }
+                    },
+                    {
+                        $project:{
+                            username:1,
+                            "avatar.url":1,
+                            subscriberCount:1,
+                            isSubscribed:1,
+                        }
+                    }
+                ]
+            }
+        },{
+            $addFields:{
+                likesCount:{
+                    $size:"$likes",
+                },
+                owner:{
+                    $first:"$owner"
+                },
+                isLiked:{
+                    $cond:{
+                        if :{$in:[req.user?._id,"$likes.likedBy"]},
+                        then : true,
+                        else : false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                "videoFile.url":1,
+                title:1,
+                description:1,
+                views:1,
+                createdAt:1,
+                duration:1,
+                comments:1,
+                owner:1,
+                likesCount:1,
+                isLiked:1
+            }
+        }
+    ])
+    if (!video) throw new ApiError(500,"Failed to fetch video");
 
+    await Video.findByIdAndUpdate({
+        $inc:{
+            views:1
+        }
+    })
+
+    await User.findByIdAndUpdate(req.user?._id,{
+        $addToSet:{
+            watchHistory:videoId,
+        }
+    })
+    return res.status(200,video,"video details fetched successfully")
 }
 const updateVideo = async(req,res)=>{
     const {title, description} = req.body;
@@ -116,7 +276,25 @@ const deleteVideo = async(req,res)=>{
     return res.status(200).json(new ApiResponse(200,videoDeleted,"Video Deleted Successfully"))
 }
 const toggelPublishStatus = async(req,res)=>{
+    const {videoId} = req.params;
+    const video = Video.findById(video);
+    if (!video) throw new ApiError(404,"Video not found");
+    
+    const toggledVideoPublished = await Video.findByIdAndUpdate(
+        videoId,
+        {
+            $set:{
+                isPublished: !video.isPublished
+            }
+        },
+        {
+            new:true
+        }
+    )
+    if (!toggledVideoPublished) throw new ApiError(500,"Could not toggle publish video");
 
+    return res.status(200).json(200,toggledVideoPublished,"Video is published toggled successfully");
+    
 }
 export {
     getAllVideos,
