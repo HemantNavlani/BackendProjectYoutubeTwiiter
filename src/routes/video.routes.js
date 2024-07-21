44a2eb171ac6a02ff4c7e46049621197da2e6b16
HemantNavlani <hemant.navlani.0506@gmail.com>
2024-07-21 14:39:48 +0530

    video routes set up done

---
+++ src/routes/video.routes.js
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import { deleteVideo, getAllVideos, getVideoById, publishAVideo, toggelPublishStatus, updateVideo } from "../controllers/video.controller";
+import { verifyJWT } from "../middlewares/auth.middleware";
+import { upload } from "../middlewares/multer.middleware";
+const router = Router();
+
+router.route('/').get(getAllVideos)
+.post(
+    verifyJWT,
+    upload.fields([
+       {
+        name:"videoFile",
+        maxCount:1
+    },
+    {
+        name:'thumbnail',
+        maxCount:1
+    }
+    ]),
+    publishAVideo
+)
+
+router
+.route('/v/:videoId')
+.get(verifyJWT,getVideoById)
+.delete(verifyJWT,deleteVideo)
+.patch(verifyJWT,upload.single('thumbnail'),updateVideo)
+
+router.route('/toggle/publish/:videoId').patch(verifyJWT,toggelPublishStatus)
+export default router;
