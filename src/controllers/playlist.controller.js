041343ff05328cf8eef77bc5766541e97215ef18
HemantNavlani <hemant.navlani.0506@gmail.com>
2024-07-22 15:07:10 +0530

    getUserPlaylists,getPlaylistsById controller made

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -104,10 +104,128 @@ const removeVideoFromPlaylist = asyncHandler(async(req,res)=>{
     return res.status(200).json(new ApiResponse(200,newPlaylist,"Video removes successfully"))
 })
 const getUserPlaylists = asyncHandler(async(req,res)=>{
-    
+    const {userId} = req.params;
+
+    const playlists = await Playlist.aggregate([
+       {
+              $match:{
+                     owner : new mongoose.Types.ObjectId(userId)
+              }
+       },
+       {
+              $lookup:{
+                     from:"videos",
+                     localField:"videos",
+                     foreignField:"_id",
+                     as:"videos"
+              }
+       },
+       {
+              $addFields:{
+                     totalVideos:{
+                            $cond: {
+                                   if: { $isArray: "$videos" }, 
+                                   then: { $size: "$videos" }, 
+                                   else: 0
+                            }      
+                     },
+                     totalViews:{
+                            $sum:"$videos.views"
+                     }
+              }
+       },
+       {
+              $project:{
+                     _id:1,
+                     name:1,
+                     description,
+                     totalVideos:1,
+                     totalViews:1,
+                     updatedAt:1,
+              }
+       }
+    ])
+
+    return res.status(200)
+    .json(200,new ApiResponse(200,playlists,"User playlists fetched successfully"))
 })
 const getPlaylistsById = asyncHandler(async(req,res)=>{
-    
+    const {playlistId} = req.params
+
+    const playlist = await Playlist.findById(playlistId);
+    if (!playlist) throw new ApiError(404,"Playlist not found");
+
+    const playlistVideos = Playlist.aggregate([
+       {
+              $match:{
+                     _id: new mongoose.Types.ObjectId(playlistId)
+              }
+       },
+       {
+              $lookup:{
+                     from:"videos",
+                     localField:"owner",
+                     foreignField:"_id",
+                     as:"videos"
+              }
+       },
+       {
+              $match:{
+                     "videos.isPublished":true
+              }
+       },
+       {
+              $lookup:{
+                     from:"users",
+                     localField:"owner",
+                     foreignField:"_id",
+                     as:"owner"
+              }
+       },
+       {
+              $addFields:{
+                     totalVideos:{
+                            $cond: {
+                                   if: { $isArray: "$videos" }, 
+                                   then: { $size: "$videos" }, 
+                                   else: 0
+                            }      
+                     },
+                     totalViews:{
+                            $sum:"$videos.views"
+                     },
+                     owner:{
+                            $first:$owner
+                     }
+              }
+       },
+       {
+              $project:{
+                     name:1,
+                     description:1,
+                     createdAt:1,
+                     updatedAt:1,
+                     totalVideos:1,
+                     totalViews:1,
+                     videos:{
+                            _id:1,
+                            "videoFile.url":1,
+                            "thumbnail.url":1,
+                            title:1,
+                            description:1,
+                            duration:1,
+                            createdAt:1,
+                            views:1,
+                     },
+                     owner:{
+                            username : 1,
+                            fullname:1,
+                            "avatar.url":1,
+                     }
+              }
+       }
+    ])
+    return res.status(200).json(new ApiResponse(200,playlistVideos,"Playlist videos fetched successfully"))
 })
 
 export {
