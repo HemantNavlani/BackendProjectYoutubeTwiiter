13ac51a2517c7d73afd6720d98e7027e04926d69
HemantNavlani <hemant.navlani.0506@gmail.com>
2024-07-22 17:33:22 +0530

    HealthCheck controller and route made

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,5 +1,6 @@
-import mongoose from 'mongoose'
-
-export {
-    
-}
+import { asyncHandler } from '../utils/asyncHandler.js'
+import { ApiResponse } from '../utils/ApiResponse.js';
+const healthCheck = asyncHandler(async(req,res)=>{
+    return res.status(200).json(new ApiResponse(200,{message:"Everything is fine"},"OK"));
+})
+export {healthCheck}
